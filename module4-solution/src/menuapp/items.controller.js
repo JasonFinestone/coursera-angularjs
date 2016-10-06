d69b5de4fdaa452563df78dbb639c731a8f98e5e
Jason <jasonfinestone@gmail.com>
2016-10-06 18:13:21 +0100

    Update

--- module4-solution/src/menuapp/items.controller.js
+++ module4-solution/src/menuapp/items.controller.js
@@ -5,11 +5,12 @@ angular.module('MenuApp')
 .controller('ItemListController', ItemListController);
 
 // 'items' is injected through state's resolve
-ItemListController.$inject = ['items','category_id']
-function ItemListController(items, category_id) {
+ItemListController.$inject = ['items']
+function ItemListController(items) {
   var itemList = this;
-  itemList.items = items;
-  itemList.category_id = category_id;
+  // I bolted on the chosen category to the service return data so that I can use it in the items template
+  itemList.items = items.menu_items;
+  itemList.category_id = items.category_id;
 }
 
 })();
