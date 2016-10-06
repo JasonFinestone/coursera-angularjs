af22c61ff1f64a96e81dc269ba46ac5a4f3a5b2b
Jason <jasonfinestone@gmail.com>
2016-10-06 18:08:30 +0100

    trying to pass chosen category id

--- module4-solution/src/menuapp/items.controller.js
+++ module4-solution/src/menuapp/items.controller.js
@@ -5,10 +5,11 @@ angular.module('MenuApp')
 .controller('ItemListController', ItemListController);
 
 // 'items' is injected through state's resolve
-ItemListController.$inject = ['items']
-function ItemListController(items) {
+ItemListController.$inject = ['items','category_id']
+function ItemListController(items, category_id) {
   var itemList = this;
   itemList.items = items;
+  itemList.category_id = category_id;
 }
 
 })();
